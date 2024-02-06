8de1a8c09f6ee754061f817e7c7c053dbebde489
leonardo <leonardocaixeta07@gmail.com>
2024-02-06 17:44:07 -0300

    feat: requisito 8

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,12 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    histories = json.loads(HistoryModel.list_as_json())
+    history = histories[0]
+
+    assert history["text_to_translate"] == "Hello, I like videogame"
+    assert history["translate_from"] == "en"
+    assert history["translate_to"] == "pt"
